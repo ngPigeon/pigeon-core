e95f7c632ff6ff75c418e27114664e53fa9acc98
alvinvoonyn <alvinvoonyn@yahoo.com>
2018-07-15 12:19:29 +0800

    First Commit

---
+++ configdb.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Database Configuration
+
+The following configuration is used to establish a MySQL database connection.
+
+HOSTNAME -> hostname of your MySQL (can be either domain name or IP address)
+USERNAME -> username of MySQL account
+PASSWORD -> password of MySQL account
+DATABASE -> Database name
+
+*/
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "db_title");
+
+?>
